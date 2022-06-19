23c84d3c0c86f89e2ce64e8b9e4e2cea30225015
Choyz <nagatoyuki1024@gmail.com>
2022-06-19 12:11:33 +0800

    feat: add message partials

--- src/routes/modules/users.js
+++ src/routes/modules/users.js
@@ -5,19 +5,30 @@ const passport = require('passport')
 const User = require('../../models/User')
 
 router.get('/login', (req, res) => {
-  res.render('login')
+  res.render('login', {
+    error: req.flash('error'),
+  })
 })
 
 router.post(
-  '/login',
+  '/login', (req, res, next) => {
+    const { email, password } = req.body
+    if (!email || !password) {
+      req.flash('warning_msg', 'Please enter Email & Password.')
+      return res.redirect('/users/login')
+    }
+    return next()
+  } ,
   passport.authenticate('local', {
     successRedirect: '/',
     failureRedirect: '/users/login',
+    failureFlash: true,
   })
 )
 
 router.get('/logout', (req, res) => {
   req.logout()
+  req.flash('success_msg', 'You have been successfully logged out.')
   res.redirect('/users/login')
 })
 
@@ -27,24 +38,40 @@ router.get('/register', (req, res) => {
 
 router.post('/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
+  const errors = []
+  if (!name || !email || !password || !confirmPassword) {
+    errors.push({ message: 'All fields are required.' })
+  }
+  if (password !== confirmPassword) {
+    errors.push({ message: 'The password confirmation does not match!' })
+  }
+  if (errors.length) {
+    return res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      confirmPassword,
+    })
+  }
   User.findOne({ email })
     .then((user) => {
       if (user) {
-        console.log('User already exists.')
-        res.render('register', {
+        errors.push({ message: 'This email is already being used.' })
+        return res.render('register', {
+          errors,
           name,
           email,
         })
-      } else {
-        return User.create({
-          name,
-          email,
-          password,
-          type: 'email',
-        })
-          .then(() => res.redirect('/'))
-          .catch((err) => console.log(err))
       }
+      return User.create({
+        name,
+        email,
+        password,
+        type: 'email',
+      })
+        .then(() => res.redirect('/'))
+        .catch((err) => console.log(err))
     })
     .catch((err) => console.log(err))
 })
