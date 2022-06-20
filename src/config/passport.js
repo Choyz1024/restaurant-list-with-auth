5ca02118778799a5e0d13313fbc3bdac5e1f3723
Choyz <nagatoyuki1024@gmail.com>
2022-06-20 12:09:18 +0800

    fix: unregistered email login

--- src/config/passport.js
+++ src/config/passport.js
@@ -12,6 +12,7 @@ module.exports = (app) => {
     new LocalStrategy({ usernameField: 'email', passReqToCallback: true }, (req, email, password, done) => {
       User.findOne({ email })
         .then((user) => {
+          if (!user) return done(null, false, { message: 'Email or Password is incorrect.' })
           return bcrypt.compare(password, user.password).then((isMatch) => {
             if (!isMatch) {
               return done(null, false, { message: 'Email or Password is incorrect.' })
